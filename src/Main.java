334b766d2c017036153a83a0d6eb6bf552a348f4
bbhardock <50248662+bbhardock@users.noreply.github.com>
2019-06-11 16:58:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String [] strings = {"Hol","a"," mundo"};
+        for (int i=0;i<3;i++) {
+            System.out.print(strings[i]);
+        }
+    }
+}
