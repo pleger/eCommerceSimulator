26b5d14dba8e1524db32f070621d56832053ed76
Paul Leger <pleger@gmail.com>
2019-06-24 11:47:44 -0400

    Update Main.java
    Comentarios al código!!

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import Agents.*;
 
 import java.util.ArrayList;
 
+// Los numeros magicos (0.3, 0.7, 10) convertirlos en constantes para entenderlos
+// formatear el codigo
+// "inNetwork" esta demas, pues siempre son inNetwork
+
 public class Main {
 
+     
     public static void main(String[] args) {
         System.out.println("Probando....");
         double [] probabilities={0.3,0.7};
         ArrayList<Buyer> buyersInNetwork= BuyerFactory.getBuyerList(10,probabilities);
         ArrayList<Market> marketsInNetwork= MarketFactory.getMarketList();
 
+        //DONDE ESTA LA COMPRA QUE RELACIONA COMPRADOR Y TIENDA 
+        
         System.out.println("Experiencia generada: ");
         ArrayList<Integer> experienceMarket=marketsInNetwork.get(0).generateExperience();
         for(int integerIndex:experienceMarket){
