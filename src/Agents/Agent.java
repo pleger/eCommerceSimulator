6244f4cccf67d25d4c8d85a261a40e250305f656
Paul Leger <pleger@gmail.com>
2019-06-24 11:54:48 -0400

    Update Agent.java

--- src/Agents/Agent.java
+++ src/Agents/Agent.java
@@ -1,6 +1,8 @@
-package Agents; //singular!!
+package Agents; //PL: singular!!
 
 //PL: no estoy seguro que sea interface!!!
+
+//PL: toda clase debería tener su toString!
 
 public interface Agent {
     void action();
